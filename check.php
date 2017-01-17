22972f652cd145d0c4c943816830e9c6aef3e9b2
mgrum <mgrum1994@gmail.com>
2017-01-17 16:28:45 +0100

    Projektstruktur erstellt

--- check.php
+++ check.php
@@ -2,7 +2,7 @@
 session_start ();
 if (! isset ( $_SESSION ['userid'] )) {
 	// Link anpassen an die entsprechende Seite
-	die ( 'Bitte zuerst einloggen<a href="account/login.html"> Login </a>' );
+	die ('Bitte zuerst einloggen<a href="account/login.php"> Login </a>');
 }
 $userid = $_SESSION ['userid'];
 ?>
