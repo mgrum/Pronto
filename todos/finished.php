f43fda98333dad46e2f72f50bd6f742097c30201
Daniel <Daniel@192.168.0.122>
2017-01-24 19:18:18 +0100

    Tabelle in finished.php eingefügt

--- todos/finished.php
+++ todos/finished.php
@@ -9,6 +9,31 @@
 <!--Include navigation-bar-->
 <?php include_once "../navigation.php" ?>
 
+<div tabelle>
+<?php
+$pdo = new PDO('mysql:host=mgrum.me;port=3306;dbname=pronto','pronto','wwi14amc');
+$project = $_SESSION ["chosenProject"];
+
+$sql = "SELECT * FROM ToDos_pro_Projekt WHERE Projekt.Bezeichnung =" . $project . " AND ToDos.Status = 'geschlossen'"; 
+
+
+echo"		
+<table>
+	<tr> <th>Bezeichnung</th> <th>Anfangszeitpunkt</th> <th>Endzeitpunkt</th> <th>Dauer</th></tr>
+	";
+ 
+foreach($pdo -> query($sql) as $row){
+	echo "<tr> <td>".$row["Bezeichnung"]."</td>";
+	echo "<td>".$row["Anfangszeitpunkt"]."</td>";
+	echo "<td>".$row["Endzeitpunkt"]."</td>";
+	echo "<td>".$row["Dauer"]."</td>";
+
+	
+}
+echo "</table>";
+?>
+</div>
+
 <!--Container for content-->
 <div class="row">
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
