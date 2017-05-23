c72d7252980e03acbcd48871281599e73cd59721
MaKoeh <marcel-koehler.94@web.de>
2017-05-23 11:44:56 +0200

    check.php
    Automatische Weiterleitung auf Login

--- check.php
+++ check.php
@@ -3,10 +3,7 @@ session_start ();
 if (! isset ( $_SESSION ['userid'] )) {
     //TODO Auf bootstrap anpassen
     die('  
-    <div class="alert alert-warning" role="alert" style="margin-top: 20px">
-        <strong>Achtung!</strong> Sie sind nicht angemeldet.
-        <a href="../account/login.php" class="alert-link"> Bitte melden Sie sich an</a>.
-    </div>    
+   header("Location:../account/login.php");
 	');
 }
 $userid = $_SESSION ['userid'];
