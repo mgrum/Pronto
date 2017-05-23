2607db250eb6dc288a4b35f2cb3918c6161047a6
mgrum <mgrum1994@gmail.com>
2017-05-23 13:59:30 +0200

    Umleitung von index auf login

--- index.php
+++ index.php
@@ -3,20 +3,15 @@
     <!--If current project is not set: "project/switch.php"-->
 <!--Else forward to "account/login.php"-->
 
-<!--Check for Login-->
-<?php include_once "check.php" ?>
- 
-
 <!-- Test ob Projekt gesetzt ist -->
 <?php
+if (!isset ($_SESSION ['userid'])) {
+    header("Location: account/login.php");
+}
 if (isset($_SESSION ['chosenProject'])) {
 		header("Location: project/dashboard.php");
 	}
 	else{
 		header("Location: project/switch.php");
 	}
-
-
-
-?> 
 
